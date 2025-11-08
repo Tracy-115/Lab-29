e351f72e882ece1fd9accf2421ffc8dcc9af0ce8
Tracy-115 <kmin984@insite.4cd.edu>
2025-11-07 20:03:54 -0800

    Update Lab-29.cpp

--- Lab-29.cpp
+++ Lab-29.cpp
@@ -15,33 +15,27 @@ int main() {
   // Initialize a map to employee 
   //key = name
   // value = progress
-  map<string, array<list<string>, 3>> employees;
+    map<string, array<list<string>, 3>> employees;
     // Open an external file to read initial data about climate zones and populate the map
         // If file does not open, print an error and exit
-  ifstream infile("tasks.txt");
-  if(!infile) {
-    cout << "Could not open file!" << endl;
-    return 1;
-  }
+    ifstream infile("tasks.txt");
+    if(!infile) {
+        cout << "Could not open file!" << endl;
+        return 1;
+    }
     // Read data from file and populate map
         // For each line, extract climate zone and species data
         // Insert species into the appropriate list in the array for their climate zone
-  string line;
-while (getline
+    string line;
+    while (getline(infile, line)){
+        //take in data like employee and task
+        //add task to list
+    }
+    infile.close();
     // Close the file
+    //going to start the output
+    cout << "Starting to Assign" << endl; //this is my starting point for output
+    workForTheDay(employees, 25);
+    cout << "Completed" << endl;
 
-    // Begin a time-based simulation for environmental changes
-        // For 20 time intervals
-            // Iterate through each climate zone in the map
-                // For each zone, simulate changes
-                    // Randomly decide if a species is to be added or removed from each category (tree, animal, flower)
-                        // If adding, generate or select a new species name to add to the list
-                        // If removing, select a random species from the list to remove
-                    // Print the changes for this interval, e.g., "Added {species} to {category} in {climate zone}"
 
-            // Simulate more complex environmental changes
-            // random events impacting the environment
-            // invasive species, deforestation, migrations
-
-            // Wait or pause briefly to simulate the passage of time between intervals
-// End of main function
