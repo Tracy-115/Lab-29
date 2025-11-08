a9598936872c49173916574b1d91cce48ceaa601
Tracy-115 <kmin984@insite.4cd.edu>
2025-11-07 20:09:59 -0800

    Update Lab-29.cpp

--- Lab-29.cpp
+++ Lab-29.cpp
@@ -12,9 +12,9 @@ void workForTheDay(map<string, array<list<string>, 3>>& employees, int Time;
 
 // Define main function
 int main() {
-  // Initialize a map to employee 
-  //key = name
-  // value = progress
+    // Initialize a map to employee 
+    //key = name
+    // value = progress
     map<string, array<list<string>, 3>> employees;
     // Open an external file to read initial data about climate zones and populate the map
         // If file does not open, print an error and exit
@@ -37,5 +37,15 @@ int main() {
     cout << "Starting to Assign" << endl; //this is my starting point for output
     workForTheDay(employees, 25);
     cout << "Completed" << endl;
-
-
+    return 0;
+}
+void workForTheDay(map<string, array<list<string>, 3>>& employees, int Time){
+    for (int t = 1; t <= Time ; ++t){
+        cout << "Time: " << t << endl;
+        for (auto& employ : employee){
+            string name = emplo.first;
+            auto& lista = employ.second; // it will start from 0, and 0 being assigned task, 1, the emoloyee is working on it, and 2 is saying completed
+            //in random, the employees will be moved around
+            //in another time, the employee will be moved along the process of assigned to progress to complete
+        cout << name << ": " << endl;
+        cout << "Task Assigned: "
